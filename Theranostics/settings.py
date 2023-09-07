2b2fa61647ebdc9f1adb8044c69cf904ee5eaa0a
sjsaluta <100128901+sjsaluta@users.noreply.github.com>
2023-09-07 22:05:39 +0800

    Modified trusted users

--- Theranostics/settings.py
+++ Theranostics/settings.py
@@ -145,6 +145,9 @@ STATICFILES_DIRS = (
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
+ALLOWED_HOSTS = ['theranostics-production.up.railway.app', '127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://theranostics-production.up.railway.app']
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = 'patientList'
