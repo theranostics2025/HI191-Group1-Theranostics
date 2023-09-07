55b95fc1beb956727dcf625d725041df23d50985
sjsaluta <100128901+sjsaluta@users.noreply.github.com>
2023-09-07 21:38:50 +0800

    Modifications

--- Theranostics/settings.py
+++ Theranostics/settings.py
@@ -91,14 +91,13 @@ WSGI_APPLICATION = 'Theranostics.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 # Password validation
