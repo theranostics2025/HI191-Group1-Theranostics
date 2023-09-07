f045fcd1bb998a0ae3493b30cf326f88f4391474
sjsaluta <100128901+sjsaluta@users.noreply.github.com>
2023-09-07 21:19:39 +0800

    Modified secret key

--- Theranostics/settings.py
+++ Theranostics/settings.py
@@ -21,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$rgwk-p!8^kw#51!zz-svqix09akmq^1nrd875l!*ei3&--m$y'
+#SECRET_KEY = 'django-insecure-$rgwk-p!8^kw#51!zz-svqix09akmq^1nrd875l!*ei3&--m$y'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-$rgwk-p!8^kw#51!zz-svqix09akmq^1nrd875l!*ei3&--m$y')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = False
